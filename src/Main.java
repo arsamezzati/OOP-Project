30565c91058534c5bd96f046c80f279fbfebda88
Stanical <arsamezzati99@gmail.com>
2023-10-16 16:56:11 +0200

    Game package created. Game and GameLogic classes created.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import Characters.Player;
+import Game.*;
+
 public class Main {
     public static void main(String[] args) {
-        Player p1 = new Player("Hila",100,50);
-        System.out.println(p1.getName());
-        System.out.println(p1.getAttackDamage());
-        System.out.println(p1.getMaxHealth());
-        System.out.println(p1.getCurHealth());
+        Game.startGame();
     }
 }
